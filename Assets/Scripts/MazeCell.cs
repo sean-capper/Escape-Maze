b5f539b6f99da2c40f76a937f3687edc8db1ab5a
Sean Capper <s1034858@monmouth.edu>
2018-02-26 23:26:30 -0500

    Added maze generation files

---
+++ Assets/Scripts/MazeCell.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeCell {
+	public int row, col;
+	public bool visited = false;
+	public GameObject northWall, southWall, eastWall, westWall, floor;
+}
